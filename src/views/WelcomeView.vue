06a69a01e2128d6d394a9d950c137c6e48277f40
Sultonkhon Oblokulov <sultondeveloper@gmail.com>
2022-11-28 17:37:13 +0500

    fix link to page

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -3,13 +3,23 @@
     <h3>
       Welcome
     </h3>
-    <router-link to="/login" class="text-blue-600">
-      Login Page
-    </router-link>
+    <div class="" v-if="!userStore.isLoggedIn">
+      <router-link to="/login" class="text-blue-600 underline">
+        Login Page
+      </router-link>
+    </div>
+    <div class="" v-else>
+      <router-link to="/main/dashboard" class="text-blue-600 underline">
+        Dashboard
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {useUserStore} from "@/store/userStore";
+
+const userStore = useUserStore()
 </script>
 
 <style></style>
