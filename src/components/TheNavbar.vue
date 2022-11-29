7686ff610cd5a3337e03c5392cb805b72ff1cf1f
Sultonkhon Oblokulov <sultondeveloper@gmail.com>
2022-11-29 09:29:53 +0500

    fix the navbar design

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,27 +1,27 @@
 <template>
-  <header class="header" v-if="userStore.isAuthenticated">
-    <div class="container mx-auto">
-      <nav class="nav py-[12px]">
+  <header class="header xl:py-[12px] xl:px-[120px]" v-if="userStore.isAuthenticated">
+    <div class="container mx-auto ">
+      <nav class="nav ">
         <ul class="nav-list flex justify-between">
-          <li class="nav-list__item">
-            <h3 class="nav-logo flex">
-              <img src="@/assets/images/logo/logo.svg" class="nav-logo__picture" alt="">
-              <span class="nav-logo__name flex flex-col">
-              METASENAT
-            </span>
-            </h3>
+          <li class="nav-list__item flex items-center">
+            <!--            <h3 class="nav-logo flex">-->
+            <!--              <img src="@/assets/images/logo/logo.svg" class="nav-logo__picture" alt="">-->
+            <!--              <span class="nav-logo__name flex flex-col">-->
+            <!--              METASENAT-->
+            <!--            </span>-->
+            <!--            </h3>-->
+            <img src="@/assets/icons/website/header-logo.svg" alt="">
           </li>
-          <li class="nav-list__item">
-            <div class="flex gap-[43px] items-center">
-              <a href="#" class="nav-user flex gap-[24px] items-center"><span class="nav-user__name">Shohrux</span> <img
-                  src="@/assets/icons/website/user.svg"
-                  class="nav-user__picture"
-                  alt="UserImage">
-              </a>
-              <button class="nav__logout" @click="userStore.logOut">
-                <span class="icon-exit"></span>
-              </button>
-            </div>
+          <li class="nav-list__item flex items-center">
+            <a href="#" class="nav-user flex items-center xl:mr-[40px]"><span
+                class="nav-user__name xl:mr-[24px] text-[13px]">Shohrux</span> <img
+                src="@/assets/icons/website/user.svg"
+                class="nav-user__picture"
+                alt="UserImage">
+            </a>
+            <button class="nav__logout " @click="userStore.logOut">
+              <img src="@/assets/icons/website/exit.svg" class="w-[26px] h-[22px]" alt="Exit icon">
+            </button>
           </li>
         </ul>
       </nav>
