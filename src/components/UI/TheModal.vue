02bc4db348feae0762def093f04a718062540e64
Sultonkhon Oblokulov <sultondeveloper@gmail.com>
2022-12-05 10:39:09 +0500

    fix typescript issue this.$emit -> $emit

--- src/components/UI/TheModal.vue
+++ src/components/UI/TheModal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="modal fixed top-0 w-auto h-auto z-[1000]">
     <div class="modal__backdrop bg-[#00000080] w-full h-screen w-screen flex justify-center items-center"
-         @click.self="this.$emit('close-modal')">
+         @click.self="$emit('close-modal')">
       <div class="modal__container min-w-[586px] bg-white">
         <slot>Default</slot>
       </div>
